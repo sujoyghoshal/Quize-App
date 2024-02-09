7a6334862325fa7c70759af57499687f6f77ebfd
sujoy ghoshal <92462200+sujoyghoshal@users.noreply.github.com>
2024-02-09 14:23:39 +0530

    Add files via upload
    Online Quiz
    
    We need to create a Quiz application where Data is stored in the backend mongo db atlas. we assume there are MCQ Questions stored in the collection, Let say store around 20 questions in Questions collections(you have to decide the attributes). The backend will have 3 collections. 
    
    Questions (20 MCQ Questions)
    Users (Empty initially)
    Attempts (Empty initially)
    
    Front End Application
    
    The Application will display a static home page, with login and signup options
    
    Signup Page will display basic information
    
     Name, Age, Gender, Roll no, password and confirm password
    
    Apply basic JavaScript validations before submitting the data.
    
    Submit the data to endpoint to receive data and store in the collections
    
    Make sure that no two users with the same Roll No exist.
    
    Login Page
    
    Login with the credentials provided, and store the session either using JWT or local storage
    
    Display a Dashboard page to the users with three options (Attempts, Start Quiz, Logout)
    
    Make sure that only logged in users have access to this page
    
    Quiz Form
    
    Start a Quiz with 5 Random questions from collection, display Question no 1 from database using DOM, JavaScript make request to endpoint to get the first question and display it using DOM
    
    Its an MCQ question so display questions along with Options to the user, and two buttons submit and next
    
    Initially next button is display , it will be enabled only when question is submitted. 
    
    Submit button will check whether the question is correct or not, and store the marks, once submitted the next button will be enabled, and next question can be displayed
    
    At last after 5 Question only submit button should be there to store the Attempts in system
    
    Attempts
    
    It Displays the attempts made by a user along with the datetime and score.
    
    Logout
    
    Clear the session/local storage and go back to home page, Make sure the Dashboard page is not available.
    
    Back end
    
    You need to create the required endpoints to manage the requests from the Front End
    
     After completing the assignment, you need to upload your project on github and share the link.

---
+++ Assessment/data/common-knowledge.js
@@ -0,0 +1,209 @@
+const quizData = {
+  title: "Quize app Application"
+};
+
+const questions = [
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is My name ?",
+    type: "mc",
+    answers: [
+      { text: "sujoy", correct: true },
+      { text: "sumit", correct: false },
+      { text: "Krish", correct: false},
+      { text: "Akash", correct: false }
+    ]
+  },
+  {
+    text: "What is my favourite place ?",
+    type: "mc",
+    answers: [
+      { text: "Delhi", correct: false },
+      { text: "Maligram,pingla", correct: true },
+      { text: "Kolkata", correct: false },
+      { text: "Mumbai", correct: false }
+    ]
+  },
+  {
+    text: "What is my favourite place ?",
+    type: "mc",
+    answers: [
+      { text: "Delhi", correct: false },
+      { text: "Maligram,pingla", correct: true },
+      { text: "Kolkata", correct: false },
+      { text: "Mumbai", correct: false }
+    ]
+  }
+
+];
+
+module.exports = { quizData, questions };
